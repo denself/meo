4ec982a982e643e654fd608000fa4cb56d1ed9df
Denis Ivanets <denself@gmail.com>
2015-05-26 00:07:30 +0300

    Count results. Correct output.

--- lab1/main.py
+++ lab1/main.py
@@ -24,15 +24,38 @@ max_rating = raw_input("Please, enter maximum rating value:\n>>> ")
 max_rating = int(max_rating)
 
 results = []
-for alt in range(number_of_alternatives):
-    results.append([None] * number_of_experts)
-    for exp in range(number_of_experts):
+for exp in range(number_of_experts):
+    results.append([None] * number_of_alternatives)
+    for alt in range(number_of_alternatives):
         val = raw_input("Expert {}, give your rating to alternative {}:\n"
                         ">>> ".format(experts[exp], alternatives[alt]))
-        results[alt][exp] = int(val)
+        results[exp][alt] = int(val)
 
-print "Original results"
+
+alternatives_summary = [None] * number_of_experts
+for exp in range(number_of_experts):
+    alternatives_summary[exp] = sum(results[exp])
+
+normalized_rating = [None] * number_of_alternatives
+for alt in range(number_of_alternatives):
+    value = 0
+    for exp in range(number_of_experts):
+        value += results[exp][alt] / float(alternatives_summary[exp])
+    value /= number_of_experts
+    normalized_rating[alt] = value
+
+
+print "Results"
 table = Texttable()
-table.add_rows([['Alternatives']+experts])
-table.add_rows(results, header=False)
+table.add_rows([['Experts'] + alternatives + ['Summary']])
+table.add_rows([[experts[row]] + results[row] + [alternatives_summary[row]] for row in range(number_of_experts)], header=False)
+table.add_rows([['Normalized'] + normalized_rating + ['']], header=False)
+
+print(table.draw())
+
+print
+print 'Rearranged results'
+table = Texttable()
+table.add_rows([['Alternative name', 'Normalized rating']])
+table.add_rows(sorted(zip(alternatives, normalized_rating), key=lambda x:x[1], reverse=True), header=False)
 print(table.draw())
